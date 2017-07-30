7c25ac77cb296a28d0eef663241ccc3ed1d99a9d
alexander.liashenko <mrmasterix@gmail.com>
2017-07-30 15:07:37 +0300

    - Framework7 added

--- js/bundle.js
+++ js/bundle.js
@@ -1,1 +1,8 @@
 import './global';
+import 'framework7';
+
+/* Framework7 css */
+import 'framework7/dist/css/framework7.ios.min.css';
+import 'framework7/dist/css/framework7.ios.colors.min.css';
+
+import './app';
---
+++ js/routes.js
@@ -0,0 +1,10 @@
+import {About} from './pages/About';
+import {Form} from './pages/Form';
+
+export const routes = [{
+  path: '/about/',
+  component: About
+}, {
+  path: '/form/',
+  component: Form
+}];
